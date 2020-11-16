836f0c95a996bd3b56b199671fc83e682be01114
Daniil Kurchanov <dkurch84@gmail.com>
2020-11-16 12:48:24 +0300

    Bump to 119

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "abix.kurjer"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 118
-        versionName = "118"
+        versionCode = 119
+        versionName = "119"
     }
     flavorDimensions("server")
     productFlavors {

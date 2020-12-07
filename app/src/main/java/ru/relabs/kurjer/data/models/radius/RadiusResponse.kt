5881bf3bd756fff042105e3cc3688cf9faecbdd1
Daniil Kurchanov <dkurch84@gmail.com>
2020-12-07 12:56:40 +0300

    #69 Fix network radius response naming

--- app/src/main/java/ru/relabs/kurjer/data/models/radius/RadiusResponse.kt
+++ app/src/main/java/ru/relabs/kurjer/data/models/radius/RadiusResponse.kt
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName
  * Created by Daniil Kurchanov on 06.01.2020.
  */
 data class RadiusResponse(
-    @SerializedName("closeAnyDistance") val closeAnyDistance: Boolean,
+    @SerializedName("closeAnyRadius") val closeAnyDistance: Boolean,
     @SerializedName("photoAnyDistance") val photoAnyDistance: Boolean,
     @SerializedName("radius") val radius: Int
 )

ae8a521cdcc2b35d07c02e60f1d1130e849a3691
Daniil Kruchanov <dkurch84@gmail.com>
2021-02-17 16:31:20 +0300

    Bump to 134

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "abix.kurjer"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 133
-        versionName = "133"
+        versionCode = 134
+        versionName = "134"
         buildConfigField("Boolean", "FEATURE_PHOTO_RADIUS", "false")
     }
     flavorDimensions("server")

ed857cacb2541d21d497c485d51445251f262dc3
Daniil Kruchanov <dkurch84@gmail.com>
2021-04-08 12:55:35 +0300

    Bump to 150

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "abix.kurjer"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 149
-        versionName = "149"
+        versionCode = 150
+        versionName = "150"
         buildConfigField("Boolean", "FEATURE_PHOTO_RADIUS", "false")
     }
     flavorDimensions("server")

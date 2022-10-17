d57a2c2e9831b80ffd59b46a011ec04c6eb6cb3e
Daniil Kruchanov <dkurch84@gmail.com>
2022-10-17 22:23:33 +0600

    Bump to 196

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "abix.kurjer"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 195
-        versionName = "195"
+        versionCode = 196
+        versionName = "196"
         buildConfigField("Boolean", "FEATURE_PHOTO_RADIUS", "true")
     }
     flavorDimensions("server")

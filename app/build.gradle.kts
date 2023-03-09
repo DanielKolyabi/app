276b5564be50971113cd0cde773cae1b5379410f
terokon <terokon59@gmail.com>
2023-03-09 13:42:26 +0300

    Подняте до версии 205

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "abix.kurjer"
         minSdkVersion(21)
         targetSdkVersion(29)
-        versionCode = 204
-        versionName = "204"
+        versionCode = 205
+        versionName = "205"
         buildConfigField("Boolean", "FEATURE_PHOTO_RADIUS", "true")
     }
     flavorDimensions("server")

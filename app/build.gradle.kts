1c8ecddd73fe1e99f7f91c76bbc96d64b6693c98
terokon <terokon59@gmail.com>
2023-05-12 12:19:24 +0300

    Поднятие версии до 211

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "abix.kurjer"
         minSdk = 21
         targetSdk = 33
-        versionCode = 210
-        versionName = "210"
+        versionCode = 211
+        versionName = "211"
         buildConfigField("Boolean", "FEATURE_PHOTO_RADIUS", "true")
     }
     flavorDimensions += listOf("server")

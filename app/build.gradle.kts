4eec91aaa4cedcb30de07796fcb02301192a13da
terokon <terokon59@gmail.com>
2023-09-14 14:08:22 +0300

    Версия 233

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "abix.kurjer"
         minSdk = 21
         targetSdk = 33
-        versionCode = 232
-        versionName = "232"
+        versionCode = 233
+        versionName = "233"
         buildConfigField("Boolean", "FEATURE_PHOTO_RADIUS", "true")
     }
     flavorDimensions += listOf("server")
